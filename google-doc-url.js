f646280e895de8458cebc214f9fb711d08757ee3
Betotri <30349332+Betotri@users.noreply.github.com>
2022-09-28 14:02:15 +0200

    Update google-doc-url.js
    New Personal API Key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XiZa2GA7Camero36XJW0IokVnymQ9f9tqCPupDzx_v0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAb3SGb5DdyhG0iWNGI5iFLAgA99g4IoWw';
